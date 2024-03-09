ba9ebc52381ae80cf4da11b5d37469473c18350d
dcdoan153 <123130804+dcdoan153@users.noreply.github.com>
2024-03-09 14:18:51 -0500

    Missile Complete

--- Assets/__Scripts/HomingMissile.cs
+++ Assets/__Scripts/HomingMissile.cs
@@ -19,16 +19,27 @@ public class HomingMissile : MonoBehaviour
     void Update()
     {
 
-        target = GameObject.FindGameObjectWithTag("Enemy").transform;
+    /*target = GameObject.FindGameObjectWithTag("Enemy").transform;
 
        if (target != null)
        {
         Vector3 direction = target.position - transform.position;
        }
 
-        transform.up =  Vector3.MoveTowards(transform.position, target.position, rotateSpeed * Time.deltaTime);
+        transform.up =  Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-       transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+       transform.position = Vector3.MoveTowards(transform.position, target.position, rotateSpeed * Time.deltaTime);*/
+        target = GameObject.FindGameObjectWithTag("Enemy").transform;
+       if (target != null)
+       {
+            Vector3 direction = target.position - rb.position;
+            direction.Normalize();
+            //Vector3 rotateAmount = Vector3.Cross(transform.forward,direction);
+            //rb.angularVelocity = rotateAmount * rotateSpeed;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, rotateSpeed * Time.deltaTime);
+            rb.velocity = transform.forward * speed;      
+       }
+       
     }
     
 }
